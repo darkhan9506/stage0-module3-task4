9b35d0ec9333427111872b97b5c0f7a3228fab2c
Darkhan Zhursin <daratechn@gmail.com>
2024-01-29 14:07:33 -0600

    done

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        int[] nums = {99, 104, 97, 114};
+
+        for (int i : nums) {
+            System.out.print((char) i);
+        }
     }
 }
